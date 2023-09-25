bd95f51ee3a83e17bba25da0d43a73a783479f01
regina <usmanova.re@yandex.ru>
2023-09-25 13:31:54 +0500

    Изменение private String url и сопутствующих элементов Доработка private void getContent() Проработка private void playGame() Проработка private void generateQuestion() Проработка private int generateWrongAnswer() Проработка private void onClickAnswer(View view) Доработка в  AndroidManifest.xml

--- app/src/main/java/com/example/riddlegame/MainActivity.java
+++ app/src/main/java/com/example/riddlegame/MainActivity.java
@@ -7,8 +7,10 @@ import android.graphics.BitmapFactory;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -17,7 +19,10 @@ import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.ArrayList;
 import java.util.concurrent.ExecutionException;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class MainActivity extends AppCompatActivity {
     private Button button1;
@@ -26,7 +31,12 @@ public class MainActivity extends AppCompatActivity {
     private Button button4;
     private ImageView imageViewPhoto;
 
-    private String url = "https://artplants.ru/rasteniya/";
+    private String url = "https://faterra.com/katalog-komnatnykh-tsvetov.html";
+    private ArrayList<String> urls;
+    private ArrayList<String> names;
+    private int numberOfQuestion;
+    private int numberOfRightAnswer;
+    private ArrayList<Button> buttons;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,20 +46,87 @@ public class MainActivity extends AppCompatActivity {
         button2 = findViewById(R.id.buttonTwo);
         button3 = findViewById(R.id.buttonThree);
         button4 = findViewById(R.id.buttonFour);
+        buttons = new ArrayList<>();
+        buttons.add(button1);
+        buttons.add(button2);
+        buttons.add(button3);
+        buttons.add(button4);
         imageViewPhoto = findViewById(R.id.imageViewPhoto);
+        urls = new ArrayList<>();
+        names = new ArrayList<>();
         getContent();
+        playGame();
     }
 
     private void getContent() {
         DownloadContentTask task = new DownloadContentTask();
         try {
             String content = task.execute(url).get();
-            Log.i("Mdfse", content);
-        } catch (ExecutionException e) {
-            e.printStackTrace();//throw new RuntimeException(e);
-        } catch (InterruptedException e) {
+            String start = "<div class=\"catalog-letter\" id=\"А\">А</div><div class=\"catalog-flowers-letter\"><div class=\"three\">";
+            String finish = "</div></div><div class=\"catalog-letter\" id=\"Б\">Б</div><div class=\"catalog-flowers-letter\"><div class=\"three\">";
+            Pattern pattern = Pattern.compile(start + "(.*?)" + finish);
+            Matcher matcher = pattern.matcher(content);
+            String splitContent = "";
+            while (matcher.find()) {
+                splitContent = matcher.group(1);
+            }
+            Pattern patternImg = Pattern.compile("src=\"(.*?)\"");
+            Pattern patternName = Pattern.compile("alt=\"(.*?)\"");
+            Matcher matcherImg = patternImg.matcher(splitContent);
+            Matcher matcherName = patternName.matcher(splitContent);
+            while (matcherImg.find()) {
+                urls.add(matcherImg.group(1));
+            }
+            while (matcherName.find()) {
+                urls.add(matcherName.group(1));
+            }
+//            for (String s : names){
+//                Log.i("JHGk", s);
+//            }
+        } catch (ExecutionException | InterruptedException e) {
             e.printStackTrace();//throw new RuntimeException(e);
         }
+    }
+
+    private void playGame() {
+        generateQuestion();
+        DownloadImgTask task = new DownloadImgTask();
+        try {
+            Bitmap bitmap = task.execute(urls.get(numberOfQuestion)).get();
+            if (bitmap != null) {
+                imageViewPhoto.setImageBitmap(bitmap);
+                for (int i = 0; i < buttons.size(); i++) {
+                    if (i == numberOfRightAnswer) {
+                        buttons.get(i).setText(names.get(numberOfQuestion));
+                    } else {
+                        int wrongAnswer = generateWrongAnswer();
+                        buttons.get(i).setText(names.get(wrongAnswer));
+                    }
+                }
+            }
+        } catch (ExecutionException | InterruptedException e) {
+            e.printStackTrace();//throw new RuntimeException(e);
+        }
+    }
+
+    private void generateQuestion() {
+        numberOfQuestion = (int) (Math.random() * names.size());
+        numberOfRightAnswer = (int) (Math.random() * buttons.size());
+    }
+
+    private int generateWrongAnswer() {
+        return (int) (Math.random() * names.size());
+    }
+
+    public void onClickAnswer(View view) {
+        Button button = (Button) view;
+        String tag = button.getTag().toString();
+        if (Integer.parseInt(tag) == numberOfRightAnswer) {
+            Toast.makeText(this, "Верно", Toast.LENGTH_SHORT).show();
+        } else {
+            Toast.makeText(this, "Ошибка, верный ответ: " + names.get(numberOfQuestion), Toast.LENGTH_SHORT).show();
+        }
+        playGame();
     }
 
     private static class DownloadContentTask extends AsyncTask<String, Void, String> {
